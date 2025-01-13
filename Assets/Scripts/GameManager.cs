cac11f34aafee8cbdbe5f15a4a7dbbb6bf19d75f
Cliffyauthor <ING23003766@heartofyorkshire.ac.uk>
2025-01-13 15:59:42 +0000

    Start
    made playground and worked on UI and camera

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public float timerlimit;
+    public float Timer;
+    public TMP_Text TimerText;
+    private float coin;
+    private float var1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Timer += Time.deltaTime;
+        var1 = (coin*100) + (Timer*10);
+        TimerText.text = ("Score:"+(var1.ToString("F1")));
+
+
+        
+    }
+}
--- Assets/Scripts/Vcam.cs
+++ Assets/Scripts/Vcam.cs
@@ -2,13 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class Vcam : MonoBehaviour
 {
     public float Yaxis;
     public float Xaxis;
-    public float followSpeed;
-    public Transform target;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +13,9 @@ public class Camera : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        Vector3 offset = new Vector3(0,5,-5);
-        transform.position = Vector3.Lerp(transform.position, target.position + offset,  followSpeed * Time.deltaTime);        
         transform.position = new Vector3(Xaxis,Yaxis , transform.position.z);
-    }
-    private void LateUpdate() 
-    {
-
-       
+        
     }
 }
